717f2b89dd14de53e452fdd59cda91f20935b89e
DDG667 <namddg667@gmail.com>
2023-04-16 10:57:15 +0800

    Packer to Lazy

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,22 +1,21 @@
 require('mason').setup()
-vim.diagnostic.config { underline = true, virtual_text = true, signs = false, severity_sort = false, }
+vim.diagnostic.config { underline = true, virtual_text = true, signs = true, severity_sort = true, }
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded", })
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
 
 local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
+    require "lsp_signature".on_attach({
+      bind = true, -- This is mandatory, otherwise border config won't get registered.
+      handler_opts = {
+        border = "rounded"
+      }
+    }, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>lk', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
@@ -90,42 +89,20 @@ local on_attach = function(client, bufnr)
 end
 
 local lsp_flags = {
-    -- This is the default in Nvim 0.7+
     debounce_text_changes = 100,
 }
--- Add additional capabilities supported by nvim-cmp
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lspconfig = require('lspconfig')
---lspconfig.setup{on_attach=require'virtualtypes'.on_attach}
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'tsserver', 'gopls', 'jsonls', 'bashls', 'clangd', 'sqlls', 'yamlls', 'html', 'cssls', 'vimls','rust_analyzer'}
+local servers = { 'tsserver', 'gopls', 'jsonls', 'bashls', 'clangd', 'sqlls', 'yamlls', 'html', 'cssls', 'vimls',
+    'rust_analyzer' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
-        -- on_attach = my_custom_on_attach,
         capabilities = capabilities,
         on_attach = on_attach,
         flags = lsp_flags,
     }
 end
---[[
-lspconfig.gopls.setup{
-	cmd = {'gopls'},
-	-- for postfix snippets and analyzers
-	capabilities = capabilities,
-	    settings = {
-	      gopls = {
-		      experimentalPostfixCompletions = true,
-		      analyses = {
-		        unusedparams = true,
-		        shadow = true,
-		     },
-		     staticcheck = true,
-		    },
-	    },
-	on_attach = on_attach,
-}]]
-    --
 
 lspconfig.pyright.setup {
     settings = {
@@ -146,47 +123,40 @@ lspconfig.pyright.setup {
 }
 
 require('lspsaga').setup({
-                           -- defaults ...
     debug = false,
     use_saga_diagnostic_sign = true,
-    -- diagnostic sign
     error_sign = "",
     warn_sign = "",
     hint_sign = "",
     infor_sign = "",
-    diagnostic_header_icon = "   ",
-    -- code action title icon
-    code_action_icon = " ",
+    diagnostic_header_icon = "",
+    code_action_icon = "",
     code_action_prompt = {
         enable = true,
         sign = true,
         sign_priority = 40,
         virtual_text = true,
     },
-    finder_definition_icon = "  ",
-    finder_reference_icon = "  ",
+    finder_definition_icon = " ",
+    finder_reference_icon = " ",
     max_preview_lines = 10,
     finder_action_keys = {
-        -- open = "o",
         open = "<CR>",
         vsplit = "s",
         split = "i",
-        -- quit = "q",
         quit = "<ESC>",
         scroll_down = "<C-f>",
         scroll_up = "<C-b>",
     },
     code_action_keys = {
-        -- quit = "q",
         quit = "<ESC>",
         exec = "<CR>",
     },
     rename_action_keys = {
-        -- quit = "<C-c>",
         quit = "<ESC>",
         exec = "<CR>",
     },
-    definition_preview_icon = "  ",
+    definition_preview_icon = "",
     border_style = "single",
     rename_prompt_prefix = "➤",
     rename_output_qflist = {
@@ -210,30 +180,9 @@ lspconfig.lua_ls.setup {
     }
 }
 
--- luasnip setup
-local luasnip = require 'luasnip'
-
--- nvim-cmp setup
-
 require('lspkind').init({
-    -- DEPRECATED (use mode instead): enables text annotations
-    --
-    -- default: true
-    -- with_text = true,
-
-    -- defines how annotations are shown
-    -- default: symbol
-    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
     mode = 'symbol_text',
-    -- default symbol map
-    -- can be either 'default' (requires nerd-fonts font) or
-    -- 'codicons' for codicon preset (requires vscode-codicons font)
-    --
-    -- default: 'default'
     preset = 'codicons',
-    -- override preset symbols
-    --
-    -- default: {}
     symbol_map = {
         Text = "",
         Method = "",
@@ -262,13 +211,8 @@ require('lspkind').init({
         TypeParameter = ""
     },
 })
---[[local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end]]
-      --
 local lspkind = require('lspkind')
+local luasnip = require('luasnip')
 local cmp = require 'cmp'
 cmp.setup {
     snippet = {
@@ -278,8 +222,7 @@ cmp.setup {
     },
     mapping = cmp.mapping.preset.insert({
         ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-        ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-        -- C-b (back) C-f (forward) for snippet placeholder navigation.
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),  -- Down
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
@@ -304,50 +247,12 @@ cmp.setup {
             end
         end, { 'i', 's' }),
     }),
-    --[[mapping={
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-      -- they way you will only jump inside the snippet region
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-["<CR>"] = cmp.mapping({
-       i = function(fallback)
-         if cmp.visible() and cmp.get_active_entry() then
-           cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-         else
-           fallback()
-       end
-       end,
-       s = cmp.mapping.confirm({ select = true }),
-       c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-     })
- },]] --
     sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'path' },
-        { name = 'nvim_lsp_signature_help' },
-        -- { name = "omni" },
-        { name = "buffer",               keyword_length = 2 },
+        --{ name = 'nvim_lsp_signature_help' },
+        { name = "buffer",                 keyword_length = 2 },
         { name = 'nvim_lua' }
     },
     formatting = {
@@ -364,22 +269,18 @@ cmp.setup {
         keyword_length = 1,
         completeopt = "menu,noselect",
     },
-    --[[  sorting = {
-        comparators = {
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            --require "cmp-under-comparator".under,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
-        },
-    },]] --
 }
--- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on(
     'confirm_done',
     cmp_autopairs.on_confirm_done()
 )
+
+
+
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,82 +1,83 @@
-require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'navarasu/onedark.nvim'
-    use 'windwp/nvim-autopairs'
-    use 'lukas-reineke/indent-blankline.nvim'
-    use 'nvim-treesitter/nvim-treesitter'
-    use 'feline-nvim/feline.nvim'
-    use 'nvim-tree/nvim-tree.lua'
-    use 'kyazdani42/nvim-web-devicons'
-    use 'ethanholz/nvim-lastplace'
-    use 'romgrk/barbar.nvim'
-    use 'p00f/nvim-ts-rainbow'
-    use 'neovim/nvim-lspconfig'
-    use 'L3MON4D3/LuaSnip'
-    use 'williamboman/mason.nvim'
-    use 'onsails/lspkind.nvim'
-    use 'glepnir/lspsaga.nvim'
-    use 'jghauser/mkdir.nvim'
-    use 'mfussenegger/nvim-lint'
-    use 'mhartington/formatter.nvim'
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use { "hrsh7th/cmp-path", after = "nvim-cmp" }
-    use { "hrsh7th/cmp-nvim-lua", after = "nvim-cmp" }
-    use { "hrsh7th/cmp-buffer", after = "nvim-cmp" }
-    --use { "hrsh7th/cmp-omni", after = "nvim-cmp" }
-    use { 'hrsh7th/cmp-nvim-lsp-signature-help', after = "nvim-cmp" }
-    use { 'saadparwaiz1/cmp_luasnip', after = "nvim-cmp" }
-    use { "lukas-reineke/cmp-under-comparator", after = "nvim-cmp" }
-    use { 'CRAG666/code_runner.nvim', requires = 'nvim-lua/plenary.nvim' }
-    use {
-        'gelguy/wilder.nvim', config = function()
-        local wilder = require('wilder')
-        wilder.setup({ modes = { ':', '/', '?' } })
-        wilder.set_option('renderer', wilder.popupmenu_renderer({
-            pumblend = 20,
-        }))
-        wilder.set_option('renderer', wilder.popupmenu_renderer({
-            -- highlighter applies highlighting to the candidates
-            highlighter = wilder.basic_highlighter(),
-        }))
-        wilder.set_option('renderer', wilder.popupmenu_renderer(
-            wilder.popupmenu_border_theme({
-                highlights = {
-                    border = 'Normal', -- highlight to use for the border
-                },
-                -- 'single', 'double', 'rounded' or 'solid'
-                -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-                border = 'rounded',
-            })
-        ))
-        wilder.set_option('renderer', wilder.popupmenu_renderer({
-            highlighter = wilder.basic_highlighter(),
-            left = { ' ', wilder.popupmenu_devicons() },
-            right = { ' ', wilder.popupmenu_scrollbar() },
-        }))
-    end
-    }
-    use {
-        'Kasama/nvim-custom-diagnostic-highlight',
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require("lazy").setup({
+    { "navarasu/onedark.nvim" },
+    { "windwp/nvim-autopairs" },
+    { "lukas-reineke/indent-blankline.nvim" },
+    { "nvim-treesitter/nvim-treesitter",    dependencies = { "p00f/nvim-ts-rainbow" } },
+    { "feline-nvim/feline.nvim" },
+    { "nvim-tree/nvim-tree.lua" },
+    { "kyazdani42/nvim-web-devicons"},
+    { "ethanholz/nvim-lastplace" },
+    { "romgrk/barbar.nvim" },
+    { "neovim/nvim-lspconfig" },
+    { "L3MON4D3/LuaSnip" },
+    { "williamboman/mason.nvim" },
+    { "onsails/lspkind.nvim" },
+    { "glepnir/lspsaga.nvim" },
+    { "mfussenegger/nvim-lint" },
+    { "mhartington/formatter.nvim" },
+    {"ray-x/lsp_signature.nvim"},
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp-signature-help",
+            "saadparwaiz1/cmp_luasnip", "lukas-reineke/cmp-under-comparator" }
+    },
+    { "CRAG666/code_runner.nvim", dependencies = "nvim-lua/plenary.nvim" },
+    {
+        "gelguy/wilder.nvim",
+        event = "VeryLazy",
         config = function()
-            require('nvim-custom-diagnostic-highlight').setup {}
+            local wilder = require("wilder")
+            wilder.setup({ modes = { ":", "/", "?" } })
+            wilder.set_option("renderer", wilder.popupmenu_renderer({
+                pumblend = 20,
+            }))
+            wilder.set_option("renderer", wilder.popupmenu_renderer({
+                -- highlighter applies highlighting to the candidates
+                highlighter = wilder.basic_highlighter(),
+            }))
+            wilder.set_option("renderer", wilder.popupmenu_renderer(
+                wilder.popupmenu_border_theme({
+                    highlights = {
+                        border = "Normal", -- highlight to use for the border
+                    },
+                    -- "single", "double", "rounded" or "solid"
+                    -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+                    border = "rounded",
+                })
+            ))
+            wilder.set_option("renderer", wilder.popupmenu_renderer({
+                highlighter = wilder.basic_highlighter(),
+                left = { " ", wilder.popupmenu_devicons() },
+                right = { " ", wilder.popupmenu_scrollbar() },
+            }))
+        end
+    },
+    {
+        "Kasama/nvim-custom-diagnostic-highlight",
+        config = function()
+            require("nvim-custom-diagnostic-highlight").setup {}
         end
     }
-    --use {'neoclide/coc.nvim', branch = 'release'}
-end)
-vim.g.python_highlight_all = 1
---[[
-vim.g.cpp_posix_standard=1
-vim.g.cpp_class_decl_highlight =1
-vim.g.cpp_member_variable_highlight=1
-vim.g.cpp_class_scope_highlight=1
-vim.g.cpp_experimental_template_highlight=1
-vim.g.cpp_concepts_highlight=1
-vim.cpp_highlight_all=1
-]]
---
+})
 
-require('pgcf')
-require('lsp')
---require('a')
---require('feline_one_monokai')
+
+
+
+require("pgcf")
+require("lsp")
